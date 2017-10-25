4ffdb3d5eb5f481554806ba2dc9dba9290553795
Hnatekmar <m.hnatma@gmail.com>
2017-10-25 10:53:46 +0200

    Added primitives and drawable (does not yet draw anything)

---
+++ Engine/Primitives/Cube.h
@@ -0,0 +1,25 @@
+//
+// Created by martin on 24.10.17.
+//
+
+#ifndef PROJECT_CUBE_H
+#define PROJECT_CUBE_H
+
+#include <GL/glew.h>
+#include "../IDrawable.h"
+
+class Cube: public IDrawable {
+    GLuint m_vbo;
+    GLuint m_shaderProgram;
+    static constexpr GLfloat s_data[] = {
+            0.0f, 0.0f, 1.0f,
+            0.5f, 1.0f, 1.0f,
+            1.0f, 0.0f, 1.0f
+    };
+public:
+    Cube();
+    void draw() override;
+};
+
+
+#endif //PROJECT_CUBE_H
