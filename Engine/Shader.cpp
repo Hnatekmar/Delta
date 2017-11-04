2ae226de64161f72845f7d0210bb5eb5e2467e43
Hnatekmar <m.hnatma@gmail.com>
2017-11-04 14:21:50 +0100

    Basic error handling for shader creation

--- Engine/Shader.cpp
+++ Engine/Shader.cpp
@@ -1,6 +1,7 @@
 #include "Shader.h"
 
 #include <fstream>
+#include <vector>
 
 GLuint compileShader(GLenum shaderType, const std::string &path) {
     GLuint shader = glCreateShader(shaderType);
@@ -13,5 +14,17 @@ GLuint compileShader(GLenum shaderType, const std::string &path) {
     auto str = fileContent.c_str();
     glShaderSource(shader, 1, &str, nullptr);
     glCompileShader(shader);
+    GLint success = 0;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if(success == GL_FALSE) {
+        GLint logSize = 0;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logSize);
+        GLchar* errorLog = new GLchar[logSize];
+        glGetShaderInfoLog(shader, logSize, &logSize, errorLog);
+        std::string persistantError(errorLog);
+        glDeleteShader(shader);
+        delete[] errorLog;
+        throw std::runtime_error("OpenGL shader compilation failed: " + persistantError);
+    }
     return shader;
 }
